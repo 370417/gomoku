d3a75a1123f9d4448d056e228e794bbca66371d9
as-f <as-f@users.noreply.github.com>
2017-04-08 11:17:26 -0500

    create basic input loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+use std::io::BufRead;
+
 fn main() {
     println!("Hello, world!");
+
+    let stdin = io::stdin();
+    read_move(&stdin);
+}
+
+fn read_move(stdin: &std::io::Stdin) {
+    for line in stdin.lock().lines() {
+        let string = line.unwrap();
+        let word = string.trim();
+
+        match word {
+            "exit" => return,
+            _ => println!("echo {}", word),
+        };
+    }
 }
