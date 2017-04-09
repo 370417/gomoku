d4e99657153416661f2fc09af500f9239d039eaa
as-f <as-f@users.noreply.github.com>
2017-04-09 03:19:46 -0500

    accept question marks at the end of inputed positions

--- src/game.rs
+++ src/game.rs
@@ -8,6 +8,13 @@ pub enum Player {
     White,
 }
 
+pub enum Move {
+    Win,
+    Tie,
+    Normal,
+    Fail, 
+}
+
 pub struct Game {
     pub player: Player,
     pub board: [[Option<Player>; SIZE as usize]; SIZE as usize],
@@ -21,17 +28,18 @@ impl Game {
         }
     }
 
-    /// Determine if (x, y) is a legal move
-    pub fn can_move(&self, x: u32, y: u32) -> bool {
-        self.board[y as usize][x as usize].is_none()
-    }
-
     /// Make the move (x, y)
-    pub fn make_move(&mut self, x: u32, y: u32) {
-        self.board[y as usize][x as usize] = Some(self.player);
+    pub fn make_move(&mut self, x: u32, y: u32) -> Move {
+        let mut piece = &mut self.board[y as usize][x as usize];
+        if piece.is_some() {
+            return Move::Fail;
+        }
+        *piece = Some(self.player);
+        println!("hi");
         self.player = match self.player {
             Player::Black => Player::White,
             Player::White => Player::Black,
         };
+        Move::Normal
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,41 +7,48 @@ use std::io;
 use game::SIZE;
 use game::Player;
 use game::Game;
+use game::Move;
 
 enum Command {
     Exit,
     Move(u32, u32),
+    Show(u32, u32),
     None,
 }
 
 fn main() {
-    println!("Hello, world!");
-
     let mut game = Game::new();
-
     let stdin = io::stdin();
     loop {
         display_game(&game);
-        let input = read_move(&stdin, "X");
-        match input {
-            Command::Exit => break,
-            Command::Move(x, y) if game.can_move(x, y) => game.make_move(x, y),
-            Command::Move(..) => println!("That intersection is already occupied"),
-            Command::None => println!("None"),
-        };
+        // let mut move_result = Move::Fail;
+        loop {
+            let mut result = Move::Fail;
+            match read_move(&stdin) {
+                Command::Exit => return,
+                Command::Move(x, y) => result = game.make_move(x, y),
+                Command::Show(x, y) => println!("{} {}", x, y),
+                Command::None => (),
+            };
+            match result {
+                Move::Fail => 0,
+                Move::Normal => break,
+                Move::Win => break,
+                Move::Tie => break,
+            };
+        }
     }
 }
 
 /// Read one move from stdin
-fn read_move(stdin: &io::Stdin, player: &str) -> Command {
+fn read_move(stdin: &io::Stdin) -> Command {
     let mut line = String::new();
-
-    println!("{}>", player);
+    println!(">>");
     stdin.read_line(&mut line).expect("Failed to read line");
-    let input = line.trim();
-    match input {
+    match line.trim() {
         "exit" | "quit" | "q" => Command::Exit,
-        _ => parse_pos(input),
+        input if is_question(input) => into_show(parse_pos(&drop(input))),
+        input => into_move(parse_pos(input)),
     }
 }
 
@@ -66,21 +73,52 @@ fn display_game(game: &Game) {
     println!();
 }
 
+/// Determine if a str ends in a '?'
+fn is_question(str: &str) -> bool {
+    match str.chars().rev().next() {
+        Some('?') => true,
+        _ => false,
+    }
+}
+
+/// Drop the last char off a string
+fn drop(str: &str) -> String {
+    let mut string = str.to_string();
+    string.pop();
+    string
+}
+
+/// Turn an optional tuple into a move command
+fn into_move(pos: Option<(u32, u32)>) -> Command {
+    match pos {
+        Some((x, y)) => Command::Move(x, y),
+        None => Command::None,
+    }
+}
+
+/// Turn an optional tuple into a show command
+fn into_show(pos: Option<(u32, u32)>) -> Command {
+    match pos {
+        Some((x, y)) => Command::Show(x, y),
+        None => Command::None,
+    }
+}
+
 /// Parse a str as a position
 ///
 /// # Examples
 ///
 /// ```
-/// assert_eq!(parse_pos("A1"), Some((0, 0)));
-/// assert_eq!(parse_pos("C10"), Some((2, 9)));
+/// assert_eq!(parse_pos("A1"), Some(Command::Move(0, 0)));
+/// assert_eq!(parse_pos("C10"), Some(Command::Move(2, 9)));
 /// assert_eq!(parse_pos("Z99"), None);
 /// assert_eq!(parse_pos("A 1"), None);
 /// assert_eq!(parse_pos("A0"), None);
 /// assert_eq!(parse_pos("a1"), None);
 /// ```
-fn parse_pos(str: &str) -> Command {
+fn parse_pos(str: &str) -> Option<(u32, u32)> {
     if str.len() < 2 {
-        return Command::None;
+        return None;
     }
     let mut chars = str.chars();
     let head = chars.next().unwrap();
@@ -88,8 +126,8 @@ fn parse_pos(str: &str) -> Command {
     let col = parse_letter(head);
     let row = parse_number(tail);
     match (col, row) {
-        (Some(x), Some(y)) => Command::Move(x, y),
-        _ => Command::None,
+        (Some(x), Some(y)) => Some((x, y)),
+        _ => None,
     }
 }
 
