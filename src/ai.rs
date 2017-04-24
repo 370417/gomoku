ea4e5481f9a234070e6ed84c260158ab3fab25af
as-f <as-f@users.noreply.github.com>
2017-04-23 19:54:54 -0500

    refactor GameTree to use Vec instead of arrays

--- src/ai.rs
+++ src/ai.rs
@@ -1,156 +1,123 @@
+use game::{SIZE, Game, Move};
+
 use rand;
 use rand::Rng;
 
-use game::{SIZE, AREA, Game, Move};
-
-// memoize heuristic?
-const EXPLORATION: f64 = 1.414213562;
-
-pub struct GameTree {
+struct GameTree {
+    move_: (i32, i32),
     wins: i32,
     games: i32,
-    children: [Node; AREA],
+    children: Vec<GameTree>,
 }
 
-type Node = Option<Box<GameTree>>;
-
 pub fn make_move(game: &Game) -> (i32, i32) {
-    let mut root = GameTree::new();
-    for _ in 0..20 {
-        root.select(&mut game.clone());
+    let mut root = GameTree {
+        move_: (-1, -1),
+        wins: 0,
+        games: 0,
+        children: Vec::new(),
+    };
+    for _ in 0..100 {
+        root.backpropagate(&mut game.clone());
     }
-    let mut games = 0;
-    let mut best_x = 0;
-    let mut best_y = 0;
-    for y in 0..SIZE {
-        for x in 0..SIZE {
-            match *root.child(x, y) {
-                Some(ref child) if child.games > games => {
-                    games = child.games;
-                    best_x = x;
-                    best_y = y;
-                },
-                Some(_) => (),
-                None => (),
-            }
+    let mut best_move = (0, 0);
+    let mut most_games = 0;
+    for child in root.children.iter() {
+        if child.games > most_games {
+            best_move = child.move_;
+            most_games = child.games;
         }
     }
-    (best_x, best_y)
+    best_move
 }
 
 impl GameTree {
-    fn new() -> GameTree {
-        GameTree {
-            wins: 0,
-            games: 0,
-            children: [
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
-            ],
-        }
-    }
 
-    fn child(&self, x: i32, y: i32) -> &Node {
-        &self.children[(x + y * SIZE) as usize]
-    }
-
-    fn mut_child(&mut self, x: i32, y: i32) -> &mut Node {
-        &mut self.children[(x + y * SIZE) as usize]
-    }
-
-    pub fn select(&mut self, game: &mut Game) -> (i32, i32) {
-        let playout = match self.get_best_move() {
-            Some((x, y)) => match game.make_move(x, y) {
-                Move::Win => (2, 2),
-                Move::Tie => (1, 2),
-                Move::Normal => match *self.mut_child(x, y) {
-                    Some(ref mut child) => child.select(game),
-                    None => panic!(),
-                },
-                Move::Fail => panic!(),
-            },
-            None => self.expand(game),
+    fn backpropagate(&mut self, game: &mut Game) -> (i32, i32) {
+        let (wins, games) = if self.children.len() > 0 {
+            let best_index = self.select();
+            let best_child = &mut self.children[best_index];
+            let (x, y) = best_child.move_;
+            game.make_move(x, y);
+            best_child.backpropagate(game)
+        } else {
+            let (wins, games, children) = self.expand(game);
+            self.children = children;
+            (wins, games)
         };
-        self.wins += 2 - playout.0;
-        self.games += playout.1;
-        playout
-    }
-
-    pub fn expand(&mut self, game: &mut Game) -> (i32, i32) {
-        let moves = get_possible_moves(game);
-        let mut wins = 0;
-        let mut games = 0;
-        let mut rng = rand::thread_rng();
-        for &(x, y) in moves.iter() {
-            let mut game_clone = game.clone();
-            let playout = match game_clone.make_move(x, y) {
-                Move::Normal => (simulate(&mut game_clone, &mut rng), 2),
-                Move::Win => (2, 2),
-                Move::Tie => (1, 2),
-                Move::Fail => panic!(),
-            };
-            let mut child = GameTree::new();
-            child.wins = playout.0;
-            child.games = playout.1;
-            self.children[(x + y * SIZE) as usize] = Some(Box::new(child));
-            wins += playout.0;
-            games += playout.1;
-        }
+        self.wins += wins;
+        self.games += games;
         (wins, games)
     }
 
-    fn get_best_move(&self) -> Option<(i32, i32)> {
-        let mut max_heuristic = 0_f64;
-        let mut best_child = self.child(0, 0);
-        let mut best_move = (0, 0);
-        for y in 0..SIZE {
-            for x in 0..SIZE {
-                match *self.child(x, y) {
-                    Some(ref child) => {
-                        let heuristic = calc_heuristic(self.games, child);
-                        if heuristic > max_heuristic {
-                            max_heuristic = heuristic;
-                            best_child = self.child(x, y);
-                            best_move = (x, y);
-                        }
-                    },
-                    None => (),
-                };
+    /// Select the best child state according to UCT.
+    /// Panic if there are no children.
+    fn select(&self) -> usize {
+        let mut best_index = 0;
+        let mut best_uct = 0_f64;
+        for (index, child) in self.children.iter().enumerate() {
+            let uct = child.uct(self.games);
+            if uct > best_uct {
+                best_index = index;
+                best_uct = uct;
             }
         }
-        match *best_child {
-            Some(..) => Some(best_move),
-            None => None,
+        best_index
+    }
+
+    fn expand(&self, game: &Game) -> (i32, i32, Vec<GameTree>) {
+        let mut rng = rand::thread_rng();
+        let mut total_wins = 0;
+        let mut total_games = 0;
+        let mut children = Vec::new();
+        for y in 0..SIZE {
+            for x in 0..SIZE {
+                let wins = simulate_move(x, y, &mut game.clone(), &mut rng);
+                if wins >= 0 {
+                    total_wins += wins;
+                    total_games += 2;
+                    children.push(GameTree {
+                        move_: (x, y),
+                        wins: wins,
+                        games: 2,
+                        children: Vec::new(),
+                    });
+                }
+            }
         }
+        (total_wins, total_games, children)
+    }
+
+    fn uct(&self, games: i32) -> f64{
+        self.wins as f64 / self.games as f64 + (2_f64 * (games as f64).ln() / self.games as f64).sqrt()
+    }
+
+}
+
+/// Simulate the outcome of a random game after a specific move is made.
+/// Return -1 if the move can't be made.
+fn simulate_move<R: Rng>(x: i32, y: i32, game: &mut Game, rng: &mut R) -> i32 {
+    match game.make_move(x, y) {
+        Move::Win => 2,
+        Move::Tie => 1,
+        Move::Normal => 2 - simulate(game, rng),
+        Move::Fail => -1,
     }
 }
 
-fn simulate(game: &mut Game, rng: &mut rand::ThreadRng) -> i32 {
-    let moves = get_possible_moves(game);
-    let index = rng.gen_range(0, moves.len());
-    let (x, y) = moves[index];
+/// Play random moves until the end of the game.
+/// Return 2 if win, 1 if tie, 0 if loss.
+fn simulate<R: Rng>(game: &mut Game, rng: &mut R) -> i32 {
+    let (x, y) = get_random_move(game, rng);
     match game.make_move(x, y) {
-        Move::Normal => 2 - simulate(game, rng),
         Move::Win => 2,
         Move::Tie => 1,
+        Move::Normal => 2 - simulate(game, rng),
         Move::Fail => panic!(),
     }
 }
 
-fn get_possible_moves(game: &Game) -> Vec<(i32, i32)> {
+fn get_random_move<R: Rng>(game: &Game, rng: &mut R) -> (i32, i32) {
     let mut moves = Vec::new();
     for y in 0..SIZE {
         for x in 0..SIZE {
@@ -159,9 +126,6 @@ fn get_possible_moves(game: &Game) -> Vec<(i32, i32)> {
             }
         }
     }
-    moves
-}
-
-fn calc_heuristic(parent_games: i32, tree: &Box<GameTree>) -> f64 {
-    tree.wins as f64 / tree.games as f64 + EXPLORATION * ((parent_games as f64).ln() / tree.games as f64).sqrt()
+    let index = rng.gen_range(0, moves.len());
+    moves[index]
 }
