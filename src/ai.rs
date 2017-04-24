5e7a621c81f68cefc9624a9578a9554033d29e29
as-f <as-f@users.noreply.github.com>
2017-04-23 20:58:27 -0500

    expand only one child at a time

--- src/ai.rs
+++ src/ai.rs
@@ -7,6 +7,7 @@ struct GameTree {
     move_: (i32, i32),
     wins: i32,
     games: i32,
+    unvisited: Vec<(i32, i32)>,
     children: Vec<GameTree>,
 }
 
@@ -15,6 +16,7 @@ pub fn make_move(game: &Game) -> (i32, i32) {
         move_: (-1, -1),
         wins: 0,
         games: 0,
+        unvisited: get_possible_moves(game),
         children: Vec::new(),
     };
     for _ in 0..100 {
@@ -33,21 +35,18 @@ pub fn make_move(game: &Game) -> (i32, i32) {
 
 impl GameTree {
 
-    fn backpropagate(&mut self, game: &mut Game) -> (i32, i32) {
-        let (wins, games) = if self.children.len() > 0 {
+    fn backpropagate(&mut self, game: &mut Game) -> i32 {
+        if self.unvisited.len() == 0 {
             let best_index = self.select();
             let best_child = &mut self.children[best_index];
             let (x, y) = best_child.move_;
             game.make_move(x, y);
-            best_child.backpropagate(game)
+            let playout = best_child.backpropagate(game);
+            self.wins += playout;
+            playout
         } else {
-            let (wins, games, children) = self.expand(game);
-            self.children = children;
-            (wins, games)
-        };
-        self.wins += wins;
-        self.games += games;
-        (wins, games)
+            self.expand(game)
+        }
     }
 
     /// Select the best child state according to UCT.
@@ -65,50 +64,40 @@ impl GameTree {
         best_index
     }
 
-    fn expand(&self, game: &Game) -> (i32, i32, Vec<GameTree>) {
+    fn expand(&mut self, game: &mut Game) -> i32 {
         let mut rng = rand::thread_rng();
-        let mut total_wins = 0;
-        let mut total_games = 0;
-        let mut children = Vec::new();
-        for y in 0..SIZE {
-            for x in 0..SIZE {
-                let wins = simulate_move(x, y, &mut game.clone(), &mut rng);
-                if wins >= 0 {
-                    total_wins += wins;
-                    total_games += 2;
-                    children.push(GameTree {
-                        move_: (x, y),
-                        wins: wins,
-                        games: 2,
-                        children: Vec::new(),
-                    });
-                }
-            }
-        }
-        (total_wins, total_games, children)
+        let (x, y) = remove_random(&mut self.unvisited, &mut rng);
+        let result = match game.make_move(x, y) {
+            Move::Win => Some(2),
+            Move::Tie => Some(1),
+            Move::Normal => None,
+            Move::Fail => panic!(),
+        };
+        let possible_moves = get_possible_moves(game);
+        let (wins, unvisited) = match result {
+            Some(x) => (x, Vec::new()),
+            None => (2 - simulate(game, &mut rng), possible_moves),
+        };
+        self.children.push(GameTree {
+            move_: (x, y),
+            wins: wins,
+            games: 2,
+            unvisited: unvisited,
+            children: Vec::new(),
+        });
+        wins
     }
 
-    fn uct(&self, games: i32) -> f64{
+    fn uct(&self, games: i32) -> f64 {
         self.wins as f64 / self.games as f64 + (2_f64 * (games as f64).ln() / self.games as f64).sqrt()
     }
 
 }
 
-/// Simulate the outcome of a random game after a specific move is made.
-/// Return -1 if the move can't be made.
-fn simulate_move<R: Rng>(x: i32, y: i32, game: &mut Game, rng: &mut R) -> i32 {
-    match game.make_move(x, y) {
-        Move::Win => 2,
-        Move::Tie => 1,
-        Move::Normal => 2 - simulate(game, rng),
-        Move::Fail => -1,
-    }
-}
-
 /// Play random moves until the end of the game.
 /// Return 2 if win, 1 if tie, 0 if loss.
 fn simulate<R: Rng>(game: &mut Game, rng: &mut R) -> i32 {
-    let (x, y) = get_random_move(game, rng);
+    let (x, y) = *choose_random(&get_possible_moves(game), rng);
     match game.make_move(x, y) {
         Move::Win => 2,
         Move::Tie => 1,
@@ -117,7 +106,7 @@ fn simulate<R: Rng>(game: &mut Game, rng: &mut R) -> i32 {
     }
 }
 
-fn get_random_move<R: Rng>(game: &Game, rng: &mut R) -> (i32, i32) {
+fn get_possible_moves(game: &Game) -> Vec<(i32, i32)> {
     let mut moves = Vec::new();
     for y in 0..SIZE {
         for x in 0..SIZE {
@@ -126,6 +115,15 @@ fn get_random_move<R: Rng>(game: &Game, rng: &mut R) -> (i32, i32) {
             }
         }
     }
-    let index = rng.gen_range(0, moves.len());
-    moves[index]
+    moves
+}
+
+fn choose_random<'a, T, R: Rng>(things: &'a Vec<T>, rng: &mut R) -> &'a T {
+    let index = rng.gen_range(0, things.len());
+    &things[index]
+}
+
+fn remove_random<T, R: Rng>(things: &mut Vec<T>, rng: &mut R) -> T {
+    let index = rng.gen_range(0, things.len());
+    things.remove(index)
 }
